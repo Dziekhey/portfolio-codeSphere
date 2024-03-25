169462e7eacd129816b8b9393f7fb8eee2c6c954
mrkabby <antwikenneth14@gmail.com>
2024-03-25 16:58:58 +0000

    added footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import './App.css'
+import About from './sections/about/About'
 import Banner from './sections/banner/Banner'
+import Blog from './sections/blogs/Blogs'
+import Footer from './sections/footer/Footer'
 
 
 
@@ -9,8 +12,7 @@ function App() {
 
   return (
     <>
-     <Navbar />
-     <Banner />
+<Blog />
     </>
   )
 }
