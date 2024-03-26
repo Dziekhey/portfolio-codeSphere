6b2fa6a7e036b5b4e89821aca139eafd0e9c070c
Dziekhey <dakeysandra@gmail.com>
2024-03-26 00:16:19 +0000

    refracted skills

--- frontend/src/components/Skills.jsx
+++ frontend/src/components/Skills.jsx
@@ -1,165 +1,26 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import { motion } from "framer-motion";
 
-const Skills = (skills) => {
+const Skills = ({ skills }) => {
   return (
-    <div className="p-8">
-        <div>
-            <h1 className="text-5xl font-medium pt-6 pb-3">Skills</h1>
-        </div>
+    <div className="">
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1, transition: { duration: 0.5 } }}
-        className="w-full flex flex-row lgl:flex-row gap-10 lgl:gap-20"
       >
-        <div className="w-1/2 lgl:w-1/2">
-          <div className="py-12 font-titleFont flex flex-col gap-4">
-            <p className="text-sm text-designColor tracking-[4px] uppercase">
-              Features
-            </p>
-            <h2 className="text-3xl md:text-4xl font-bold">Programming Languages and Liberies</h2>
-          </div>
-          <div className='className="mt-14 w-full flex flex-col gap-6'>
+        <div className="lgl:w-1/2">
+          <div className="mb-14 flex flex-col gap-6">
             <div className="overflow-x-hidden">
-              <p className="text-sm uppercase font-medium">HTML</p>
+              <p className="text-sm uppercase font-medium">{skills.skill}</p>
               <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
                 <motion.span
                   initial={{ x: "-100%", opacity: 0 }}
                   animate={{ x: 0, opacity: 1 }}
                   transition={{ duration: 0.5, delay: 0.5 }}
-                  className="w-[80%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+                  className={`w-[${skills.proficiency}] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative`}
                 >
-                  <span className="absolute -top-7 right-0">80%</span>
-                </motion.span>
-              </span>
-            </div>
-            <div className="overflow-x-hidden">
-              <p className="text-sm uppercase font-medium">CSS</p>
-              <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
-                <motion.span
-                  initial={{ x: "-100%", opacity: 0 }}
-                  animate={{ x: 0, opacity: 1 }}
-                  transition={{ duration: 0.5, delay: 0.5 }}
-                  className="w-[60%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
-                >
-                  <span className="absolute -top-7 right-0">60%</span>
-                </motion.span>
-              </span>
-            </div>
-            <div className="overflow-x-hidden">
-              <p className="text-sm uppercase font-medium">JAVASCRIPT</p>
-              <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
-                <motion.span
-                  initial={{ x: "-100%", opacity: 0 }}
-                  animate={{ x: 0, opacity: 1 }}
-                  transition={{ duration: 0.5, delay: 0.5 }}
-                  className="w-[50%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
-                >
-                  <span className="absolute -top-7 right-0">50%</span>
-                </motion.span>
-              </span>
-            </div>
-            <div className="overflow-x-hidden">
-              <p className="text-sm uppercase font-medium">BOOTSTRAP</p>
-              <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
-                <motion.span
-                  initial={{ x: "-100%", opacity: 0 }}
-                  animate={{ x: 0, opacity: 1 }}
-                  transition={{ duration: 0.5, delay: 0.5 }}
-                  className="w-[70%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
-                >
-                  <span className="absolute -top-7 right-0">70%</span>
-                </motion.span>
-              </span>
-            </div>
-            <div className="overflow-x-hidden">
-              <p className="text-sm uppercase font-medium">TAILWIND</p>
-              <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
-                <motion.span
-                  initial={{ x: "-100%", opacity: 0 }}
-                  animate={{ x: 0, opacity: 1 }}
-                  transition={{ duration: 0.5, delay: 0.5 }}
-                  className="w-[70%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
-                >
-                  <span className="absolute -top-7 right-0">70%</span>
-                </motion.span>
-              </span>
-            </div>
-          </div>
-        </div>
-
-        <div className="w-1/2 lgl:w-1/2">
-          <div className="py-12 font-titleFont flex flex-col gap-4">
-            <p className="text-sm text-designColor tracking-[4px] uppercase">
-              Features
-            </p>
-            <h2 className="text-3xl md:text-4xl font-bold">
-              Software Proficiency and Frameworks
-            </h2>
-          </div>
-          <div className="flex flex-col gap-6">
-            <div className="overflow-x-hidden">
-              <p className="text-sm uppercase font-medium">VISUAL STUDIO CODE</p>
-              <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
-                <motion.span
-                  initial={{ x: "-100%", opacity: 0 }}
-                  animate={{ x: 0, opacity: 1 }}
-                  transition={{ duration: 0.5, delay: 0.5 }}
-                  className="w-[60%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
-                >
-                  <span className="absolute -top-7 right-0">60%</span>
-                </motion.span>
-              </span>
-            </div>
-            <div className="overflow-x-hidden">
-              <p className="text-sm uppercase font-medium">GIT</p>
-              <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
-                <motion.span
-                  initial={{ x: "-100%", opacity: 0 }}
-                  animate={{ x: 0, opacity: 1 }}
-                  transition={{ duration: 0.5, delay: 0.5 }}
-                  className="w-[60%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
-                >
-                  <span className="absolute -top-7 right-0">60%</span>
-                </motion.span>
-              </span>
-            </div>
-            <div className="overflow-x-hidden">
-              <p className="text-sm uppercase font-medium">JIRA</p>
-              <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
-                <motion.span
-                  initial={{ x: "-100%", opacity: 0 }}
-                  animate={{ x: 0, opacity: 1 }}
-                  transition={{ duration: 0.5, delay: 0.5 }}
-                  className="w-[50%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
-                >
-                  <span className="absolute -top-7 right-0">50%</span>
-                </motion.span>
-              </span>
-            </div>
-            <div className="overflow-x-hidden">
-              <p className="text-sm uppercase font-medium">REACT.JS</p>
-              <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
-                <motion.span
-                  initial={{ x: "-100%", opacity: 0 }}
-                  animate={{ x: 0, opacity: 1 }}
-                  transition={{ duration: 0.5, delay: 0.5 }}
-                  className="w-[50%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
-                >
-                  <span className="absolute -top-7 right-0">50%</span>
-                </motion.span>
-              </span>
-            </div>
-            <div className="overflow-x-hidden">
-              <p className="text-sm uppercase font-medium">NODE.JS</p>
-              <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
-                <motion.span
-                  initial={{ x: "-100%", opacity: 0 }}
-                  animate={{ x: 0, opacity: 1 }}
-                  transition={{ duration: 0.5, delay: 0.5 }}
-                  className="w-[50%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
-                >
-                  <span className="absolute -top-7 right-0">50%</span>
+                  <span className="absolute -top-7 right-0">{skills.proficiency}</span>
                 </motion.span>
               </span>
             </div>
--- frontend/src/pages/Individual/ComfortPage.jsx
+++ frontend/src/pages/Individual/ComfortPage.jsx
@@ -1,14 +1,16 @@
-import React ,{useEffect, useState} from 'react'
-import Compic from '../../assets/Compic.jpg';
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import Compic from "../../assets/Compic.jpg";
 import ImgCard from "../../components/ImgCard.jsx";
 import Skills from "../../components/Skills.jsx";
 import Experiences from "../../components/Experiences.jsx";
 import "./individual.css";
-import { apiGetAllSkills, apiUserSkills } from '../../services/skills.services.js';
-
+import {
+  apiGetAllSkills,
+  apiUserSkills,
+} from "../../services/skills.services.js";
 
 const ComfortPage = () => {
-
   const ExperiencesData = [
     {
       type: "Education",
@@ -52,79 +54,134 @@ const ComfortPage = () => {
     },
   ];
 
-  const [comskills, setComskills] = useState([])
+  const SkillData = [
+    {
+      skill: "Javascript",
+      proficiency: "80%",
+    },
+    {
+      skill: "Javascript",
+      proficiency: "80%",
+    },
+    {
+      skill: "Javascript",
+      proficiency: "80%",
+    },
+    {
+      skill: "Javascript",
+      proficiency: "80%",
+    },
+  ];
+
+  const [comskills, setComskills] = useState([]);
   const [loading, setLoading] = useState(false);
 
   const fetchData = async () => {
     try {
       const skills = await apiGetAllSkills();
       console.log(skills.data);
-      setComskills(skills.data)
+      setComskills(skills.data);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   return (
     <div id="bg">
-    {/* About division */}
-    <div className="flex justify-center items-center">
-      <h1 className="text-7xl font-extrabold py-6">ABOUT COMFORT</h1>
-    </div>
-    <div className="flex flex-row">
-      <div className="shrink-0 w-96">
-        <ImgCard Image={Compic} />
+      {/* About division */}
+      <div className="flex justify-center items-center">
+        <h1 className="text-7xl font-extrabold py-6">ABOUT COMFORT</h1>
       </div>
-      <div className="shrink w-150 flex text-center">
-        <p className="py-20 px-10">
-          Hello! I'm Sandra, an alumnus of the University of Ghana, Legon,
-          where I majored in Nutrition and Food Science. Currently, I'm
-          embarking on an exciting journey as a web developer trainee at MEST
-          Africa. This dynamic blend of my academic background and current
-          pursuit in technology reflects my passion for interdisciplinary
-          exploration and innovation. With a solid foundation in nutrition
-          science, I bring a unique perspective to the realm of web
-          development, striving to merge these diverse fields to create
-          meaningful digital solutions. My overarching goal is to carve a
-          fulfilling career path in web development, leveraging my
-          comprehensive skill set to contribute positively to the
-          ever-evolving tech landscape. Beyond coding, I'm deeply committed to
-          continuous learning and community engagement, believing in the
-          transformative power of collaboration. I am enthusiastic about
-          connecting with like-minded individuals who share my zeal for growth
-          and exploration. Join me on this exhilarating journey as we navigate
-          the boundless opportunities of technology, driven by curiosity,
-          creativity, and a relentless pursuit of excellence.
-        </p>
+      <div className="flex flex-row">
+        <div className="shrink-0 w-96">
+          <ImgCard Image={Compic} />
+        </div>
+        <motion.div
+  animate={{ x: [0, 100, 0] }}
+>
+        <div className="shrink w-150 flex text-center">
+          <p className="py-20 px-10">
+            Hello! I'm Sandra, an alumnus of the University of Ghana, Legon,
+            where I majored in Nutrition and Food Science. Currently, I'm
+            embarking on an exciting journey as a web developer trainee at MEST
+            Africa. This dynamic blend of my academic background and current
+            pursuit in technology reflects my passion for interdisciplinary
+            exploration and innovation. With a solid foundation in nutrition
+            science, I bring a unique perspective to the realm of web
+            development, striving to merge these diverse fields to create
+            meaningful digital solutions. My overarching goal is to carve a
+            fulfilling career path in web development, leveraging my
+            comprehensive skill set to contribute positively to the
+            ever-evolving tech landscape. Beyond coding, I'm deeply committed to
+            continuous learning and community engagement, believing in the
+            transformative power of collaboration. I am enthusiastic about
+            connecting with like-minded individuals who share my zeal for growth
+            and exploration. Join me on this exhilarating journey as we navigate
+            the boundless opportunities of technology, driven by curiosity,
+            creativity, and a relentless pursuit of excellence.
+          </p>
+        </div>
+        </motion.div>
       </div>
-    </div>
 
-    {/* Skills division */}
-    <Skills />
-
-    {/* Experience division */}
-    <Experiences experiences={ExperiencesData} />
-
-    {/* Contact division*/}
-    <div className="p-8 border-t-4 border-black">
-      <div className="text-4xl font-bold pb-3">Contact Me</div>
-      <div className="flex justify-between font-medium py-4">
-      <span>email</span>
-      <span>telephone</span>
-      <span>location</span>
+      {/* Skills division */}
+      <div className="p-8">
+        <h1 className="text-5xl font-medium pt-8">Skills</h1>
+        <div className="w-full flex lgl:flex-row gap-10 lgl:gap-20">
+          <div className="w-1/2 lgl:w-1/2">
+            <div className="py-12 font-titleFont flex flex-col gap-4">
+              <p className="text-sm text-designColor tracking-[4px] uppercase">
+                Features
+              </p>
+              <h2 className="text-3xl md:text-4xl font-bold">
+                Programming Languages and Liberies
+              </h2>
+            </div>
+            <div className="w-full lgl:w-1/2">
+            {
+              SkillData.length>0 && SkillData.map((data, index) => (<Skills key={index} skills={data}/>))
+            }
+            </div>
+          </div>
+          <div className="w-1/2 lgl:w-1/2">
+            <div className="py-12 font-titleFont flex flex-col gap-4">
+              <p className="text-sm text-designColor tracking-[4px] uppercase">
+                Features
+              </p>
+              <h2 className="text-3xl md:text-4xl font-bold">
+                Software Proficiency and Frameworks
+              </h2>
+            </div>
+            {
+              SkillData.length>0 && SkillData.map((data, index) => (<Skills key={index} skills={data}/>))
+            }
+          </div>
+        </div>
       </div>
-      <div className="flex justify-center space-x-10 font-medium py-6">
-      <span>Whatsapp</span>
-      <span>Instagram</span>
-      <span>LinkedIn</span>
-      <span>github</span>
+    
+      {/* Experience division */}
+      <Experiences experiences={ExperiencesData} />
+
+      {/* Contact division*/}
+      <div className="p-8 border-t-4 border-black">
+        <div className="text-4xl font-bold pb-3">Contact Me</div>
+        <div className="flex justify-between font-medium py-4">
+          <span>email</span>
+          <span>telephone</span>
+          <span>location</span>
+        </div>
+        <div className="flex justify-center space-x-10 font-medium py-6">
+          <span>Whatsapp</span>
+          <span>Instagram</span>
+          <span>LinkedIn</span>
+          <span>github</span>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
 export default ComfortPage;
