ce81fe7556023ab5cf485f9fb79db9da830b006c
mrkabby <antwikenneth14@gmail.com>
2024-03-28 12:59:02 +0000

    no app jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,13 @@
 
 import './App.css'; 
-import About from './sections/about/About';
-import Banner from './sections/banner/Banner';
-import Footer from './sections/footer/Footer';
-import Navbar from './sections/navbar/Navbar';
-import Projects from './sections/projects/projects';
+
 
 
 
 const App = () => {
   return (
     <>
-<Navbar />
-<Banner />
-<About />
-<Projects/>
-<Footer />
+
     </>
     
   )
